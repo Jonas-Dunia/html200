336405643598eff6927bf5e26b2820d14ebf3c92
Jonas Wabenga <jwabenga@gmail.com>
2025-05-04 19:24:01 -0700

    I added calculator.js with variable math operations

---
+++ lesson03/calculator.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 5;
+let c = 2;
+let d = 8;
+
+console.log(a + b); // Addition
+console.log(d - c); // Subtraction
+console.log(a * c); // Multiplication
+console.log(d / b); // Division
+console.log((a + d) * c); // Mixed operation
