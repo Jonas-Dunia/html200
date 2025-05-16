ba454ecd8c76fb598f6d59f774073bb6aea57002
Jonas Wabenga <jwabenga@gmail.com>
2025-05-15 19:41:29 -0700

    Added exercise-three.js for challenge 3

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,7 @@
+// Create an array with at least 5 strings
+let animals = ["cats", "dogs", "birds", "fish", "lizards"];
+
+// Use a for loop to log index and value
+for (let i = 0; i < animals.length; i++) {
+  console.log((i + 1) + "," + animals[i]);
+}
