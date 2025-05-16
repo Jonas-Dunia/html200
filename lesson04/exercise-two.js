cf8e32bb50fc9c5921db76ff20344232ef2481f3
Jonas Wabenga <jwabenga@gmail.com>
2025-05-15 19:23:30 -0700

    Added exercise-two.js for challenge 2

---
+++ lesson04/exercise-two.js
@@ -0,0 +1,8 @@
+// Take a sentence of at least five words and a period
+let sentence = "Learning JavaScript can be fun.";
+
+// Split the sentence into an array of words
+let words = sentence.split(" ");
+
+// Console.log the third word (index 2)
+console.log(words[2]);
